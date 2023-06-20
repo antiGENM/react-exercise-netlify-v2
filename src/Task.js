0e8a4dd034deb908d84bd36229efc598545aa46c
antiGENM <lukasbelipati@students.unnes.ac.id>
2023-06-20 18:20:37 +0700

    initial commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Mario E. Belipati" // TODO: replace this
+const studentId = "FE5426745" // TODO: replace this
+
+export { url, studentName, studentId }
