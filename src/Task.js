3af73f46f8eb84b4330762096242540a83d8e6ee
antiGENM <lukasbelipati@students.unnes.ac.id>
2023-06-20 18:26:15 +0700

    replace netlify url

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://wondrous-lamington-c7fd63.netlify.app/" // TODO: replace this
 const studentName = "Mario E. Belipati" // TODO: replace this
 const studentId = "FE5426745" // TODO: replace this
 
